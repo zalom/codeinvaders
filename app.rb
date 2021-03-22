e7ca4056669544ad2caf00c014faa110fc059240
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2021-03-22 12:34:34 +0100

    Remove commented out code

--- app.rb
+++ app.rb
@@ -11,19 +11,3 @@ require './lib/services/output_service'
 require './lib/code_invaders'
 
 CodeInvaders.run
-
-# positions_sorted = invader2_positions.sort_by { |e| e[:columns].start && e[:rows].start }
-#
-# remap = positions_sorted.map { |has| has.transform_values { |position| position.range.to_a } }
-# [].tap do |filtered|
-#   remap.each do |pos|
-#     remap.each do |posc|
-#       filtered << posc if pos[:x].difference(posc[:x]).size == 8
-#     end
-#   end
-# end
-# remap.sort_by { |k, v| k[:y] }
-#
-# invader1_positions.map do |position|
-#   position if uncovered?(invader1_positions, position)
-# end
